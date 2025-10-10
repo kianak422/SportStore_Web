88781dbb58c4a560316760fa03a4da959ede7b45
kianak422 <huyway2005@gmail.com>
2025-10-10 09:06:22 +0000

    Mô tả thay đổi

--- ltwncbuoi1-main/SportsSln/SportsStore/obj/Debug/net6.0/SportsStore.AssemblyInfo.cs
+++ ltwncbuoi1-main/SportsSln/SportsStore/obj/Debug/net6.0/SportsStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ce1b5836b575a8f226c1b0255d357719d2295e44")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SportsStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
